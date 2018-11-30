9c70cb17deaaa48db6824ad63471ad13b437bfc8
Chen, Michael <Michael_Chen@cable.comcast.com>
2018-11-29 23:57:56 -0500

    problem with babel minify, don't use

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const merge = require("webpack-merge");
-const MinifyPlugin = require("babel-minify-webpack-plugin");
 const common = require("./webpack.common");
 const package = require("./package.json");
 
@@ -8,6 +7,5 @@ module.exports = merge(common, {
   output: {
     filename: package.name + ".min.js"
   },
-  devtool: "source-map",
-  plugins: [new MinifyPlugin()]
+  devtool: "source-map"
 });
